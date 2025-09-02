18667ab42d37fd5f5937d0ae41d354c4067449da
Amit Dangre <amit.dangre@gmail.com>
2025-09-02 09:22:33 +0200

    I've updated the for.py file

--- for.py
+++ for.py
@@ -1,14 +1,14 @@
-# exp=[2340, 2500, 2100, 3100, 2980]
-# # total= exp[0]+exp[1]+exp[2]+exp[3]
-total=0
-# for i in exp:
-#     total=total+i
-# print("total expense is:",total)
-# for i in range(2,10):
-#     total = total + 1
-#     print("number is:", i, "The square is:", i*i)
-#
-# print(total)
+exp = [2340, 2500, 2100, 3100, 2980]
+total = exp[0] + exp[1] + exp[2] + exp[3]
+total = 0
+for i in exp :
+    total = total + i
+print("total expense is:", total)
+for i in range(2, 10) :
+    total = total + 1
+    print("number is:", i, "The square is:", i * i)
+
+print(total)
 
 key_location = "chair"
 locations = ["garage", "living room", "chair", "closet"]

4370825b6feb6a55216a93ec8e01e804282d4ad5
Amit Dangre <amit.dangre@gmail.com>
2025-09-03 04:03:54 +0200

    feature release 2.0

--- Hello.py
+++ Hello.py
@@ -1,1 +1,5 @@
 print('hello')
+myInput = input("Enter your message:")
+print("The message is",myInput)
+# This is the changes for the feature_release2.0
+print("The feature release 2.0 is also important for us to test the necessary changes")
